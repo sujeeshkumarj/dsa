ae535cdc136ca6bc361e2cfd423d2dcf3e536b7f
sujeeshkumarj <sujeesh.ambadi1999@gmail.com>
2019-11-20 19:36:21 +0530

    fibonacci using recursion

--- ip1fibrec.c
+++ ip1fibrec.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 #include<time.h>
 int Fibr(int);
-
+void exe()
+{
+    printf("exe() starts \n");
+    printf("Press enter to stop exe \n");
+    while(1)
+    {
+        if (getchar())
+            break;
+    }
+    printf("exe() ends \n");
+}
 int main()
 {
     clock_t t;
     t=clock();
-    int Fibr(int);
+    exe();
     t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC;
 
@@ -21,7 +31,7 @@ int main()
         printf("%d\n", Fibr(i));
         i++;
     }
-    printf("Fibr() took %f seconds to execute \n", time_taken);
+      printf("exe() took %f seconds to execute \n", time_taken);
 
     return 0;
 }
